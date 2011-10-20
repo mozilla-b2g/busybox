41f2ed9b0f51648b2ce0ed7cf5439d2bee6e9b87
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-10-20 11:58:55 +0159

    busybox: respect "make showcommands"
    This passes V=1 to busybox's Makefiles if showcommands is being used,
    allowing showcommands to work as expected.
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- Android.mk
+++ Android.mk
@@ -5,6 +5,9 @@ BB_TC_PREFIX := $(shell basename $(TARGET_TOOLS_PREFIX))
 BB_LDFLAGS := -Xlinker -z -Xlinker muldefs -nostdlib -Bdynamic -Xlinker -T../../$(BUILD_SYSTEM)/armelf.x -Xlinker -dynamic-linker -Xlinker /system/bin/linker -Xlinker -z -Xlinker nocopyreloc -Xlinker --no-undefined ../../$(TARGET_CRTBEGIN_DYNAMIC_O) ../../$(TARGET_CRTEND_O) -L../../$(TARGET_OUT_STATIC_LIBRARIES)
 BB_COMPILER_FLAGS := $(subst -I ,-I../../,$(subst -include ,-include ../../,$(TARGET_GLOBAL_CFLAGS))) -I../../bionic/libc/include -I../../bionic/libc/kernel/common -I../../bionic/libc/arch-arm/include -I../../bionic/libc/kernel/arch-arm -I../../bionic/libm/include -fno-stack-protector -Wno-error=format-security
 BB_LDLIBS := dl m c gcc
+ifneq ($(strip $(SHOW_COMMANDS)),)
+BB_VERBOSE="V=1"
+endif
 
 .PHONY: busybox
 
@@ -17,6 +20,6 @@ busybox: $(TARGET_CRTBEGIN_DYNAMIC_O) $(TARGET_CRTEND_O) $(TARGET_OUT_STATIC_LIB
 	sed -e "s|^CONFIG_CROSS_COMPILER_PREFIX=.*|CONFIG_CROSS_COMPILER_PREFIX=\"$(BB_TC_PREFIX)\"|;s|^CONFIG_EXTRA_CFLAGS=.*|CONFIG_EXTRA_CFLAGS=\"$(BB_COMPILER_FLAGS)\"|" configs/android_defconfig >.config && \
 	export PATH=$(BB_TC_DIR):$(PATH) && \
 	$(MAKE) oldconfig && \
-	$(MAKE) EXTRA_LDFLAGS="$(BB_LDFLAGS)" LDLIBS="$(BB_LDLIBS)" && \
+	$(MAKE) $(BB_VERBOSE) EXTRA_LDFLAGS="$(BB_LDFLAGS)" LDLIBS="$(BB_LDLIBS)" && \
 	mkdir -p ../../$(PRODUCT_OUT)/system/bin && \
 	cp busybox ../../$(PRODUCT_OUT)/system/bin/
